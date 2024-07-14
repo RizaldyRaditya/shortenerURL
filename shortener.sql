ba22c6282bfc71f74ab4543a314fbc48e40170c3
RizaldyRaditya <rizaldyradityae26@gmail.com>
2024-07-14 15:57:49 +0700

    no message

---
+++ shortener.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 29, 2020 at 10:22 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `urlshortener`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shortener`
+--
+
+CREATE TABLE `shortener` (
+  `id` int(11) NOT NULL,
+  `longURL` varchar(500) NOT NULL,
+  `shortURL` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `shortener`
+--
+
+INSERT INTO `shortener` (`id`, `longURL`, `shortURL`) VALUES
+(2, 'https://www.youtube.com/watch?v=RkmvVFZJJvs', 'wvccr'),
+(3, 'https://www.youtube.com/watch?v=NHg6jQajaMs', 'su327'),
+(4, 'https://www.youtube.com/watch?v=RkmvVFZJJvs', 'fxki2');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `shortener`
+--
+ALTER TABLE `shortener`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `shortener`
+--
+ALTER TABLE `shortener`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
